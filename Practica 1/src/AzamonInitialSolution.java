8ebbb715f71b2b21915058cc944d749ead4e8561
maca <macarena.prado@estudiantat.upc.edu>
2024-10-09 12:13:44 +0200

    Adding fill capacity method

--- Practica 1/src/AzamonInitialSolution.java
+++ Practica 1/src/AzamonInitialSolution.java
@@ -1,23 +1,41 @@
+import IA.Azamon.Oferta;
 import IA.Azamon.Paquete;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class AzamonInitialSolution {
 
-    private ArrayList<Paquete> capacityFirstSolution(ArrayList<Paquete> packages) {
+    public HashMap<Oferta, ArrayList<Paquete>> capacityFirstSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingDouble(Paquete::getPeso));
-        return packages;
+        return fillCapacity(offers, packages);
     }
 
-    private ArrayList<Paquete> priorityFirstSolution(ArrayList<Paquete> packages) {
+    public HashMap<Oferta, ArrayList<Paquete>> priorityFirstSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingInt(Paquete::getPrioridad));
-        return packages;
+        return fillCapacity(offers, packages);
     }
 
-    private ArrayList<Paquete> randomSolution(ArrayList<Paquete> packages) {
+    public HashMap<Oferta, ArrayList<Paquete>> randomSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
         Collections.shuffle(packages);
-        return packages;
+        return fillCapacity(offers, packages);
+    }
+
+    private HashMap<Oferta, ArrayList<Paquete>> fillCapacity(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages){
+        for(Oferta o : offers.keySet()) {
+            double maxWeight = o.getPesomax();
+            double currentWeight = 0;
+            for(Paquete p : packages) {
+                if(currentWeight + p.getPeso() <= maxWeight) {
+                    offers.get(o).add(p);
+                    currentWeight += p.getPeso();
+                    packages.remove(p);
+                }
+            }
+        }
+
+        return offers;
     }
 }
