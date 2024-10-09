6c284fe9b9a6a6648df6b9bdd4bf184981f588f6
maca <macarena.prado@estudiantat.upc.edu>
2024-10-09 12:56:11 +0200

    Fixing HashMap

--- Practica 1/src/AzamonInitialSolution.java
+++ Practica 1/src/AzamonInitialSolution.java
@@ -8,28 +8,28 @@ import java.util.HashMap;
 
 public class AzamonInitialSolution {
 
-    public HashMap<Oferta, ArrayList<Paquete>> capacityFirstSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
+    public HashMap<Oferta, Paquete> capacityFirstSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingDouble(Paquete::getPeso));
         return fillCapacity(offers, packages);
     }
 
-    public HashMap<Oferta, ArrayList<Paquete>> priorityFirstSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
+    public HashMap<Oferta, Paquete> priorityFirstSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingInt(Paquete::getPrioridad));
         return fillCapacity(offers, packages);
     }
 
-    public HashMap<Oferta, ArrayList<Paquete>> randomSolution(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages) {
+    public HashMap<Oferta, Paquete> randomSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
         Collections.shuffle(packages);
         return fillCapacity(offers, packages);
     }
 
-    private HashMap<Oferta, ArrayList<Paquete>> fillCapacity(HashMap<Oferta, ArrayList<Paquete>> offers, ArrayList<Paquete> packages){
+    private HashMap<Oferta, Paquete> fillCapacity(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages){
         for(Oferta o : offers.keySet()) {
             double maxWeight = o.getPesomax();
             double currentWeight = 0;
             for(Paquete p : packages) {
                 if(currentWeight + p.getPeso() <= maxWeight) {
-                    offers.get(o).add(p);
+                    offers.put(o,p);
                     currentWeight += p.getPeso();
                     packages.remove(p);
                 }
