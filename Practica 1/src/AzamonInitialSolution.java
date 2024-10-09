13e88629afe2b4a505bb8689df08ff6e59ced237
maca <macarena.prado@estudiantat.upc.edu>
2024-10-09 14:00:14 +0200

    Changing to object arraylist

--- Practica 1/src/AzamonInitialSolution.java
+++ Practica 1/src/AzamonInitialSolution.java
@@ -4,38 +4,40 @@ import IA.Azamon.Paquete;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 
 public class AzamonInitialSolution {
 
-    public HashMap<Oferta, Paquete> capacityFirstSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
+    public ArrayList<Object> capacityFirstSolution(ArrayList<Oferta> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingDouble(Paquete::getPeso));
         return fillCapacity(offers, packages);
     }
 
-    public HashMap<Oferta, Paquete> priorityFirstSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
+    public ArrayList<Object> priorityFirstSolution(ArrayList<Oferta> offers, ArrayList<Paquete> packages) {
         packages.sort(Comparator.comparingInt(Paquete::getPrioridad));
         return fillCapacity(offers, packages);
     }
 
-    public HashMap<Oferta, Paquete> randomSolution(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages) {
+    public ArrayList<Object> randomSolution(ArrayList<Oferta> offers, ArrayList<Paquete> packages) {
         Collections.shuffle(packages);
         return fillCapacity(offers, packages);
     }
 
-    private HashMap<Oferta, Paquete> fillCapacity(HashMap<Oferta, Paquete> offers, ArrayList<Paquete> packages){
-        for(Oferta o : offers.keySet()) {
+    private ArrayList<Object> fillCapacity(ArrayList<Oferta> offers, ArrayList<Paquete> packages){
+        ArrayList<Object> result = new ArrayList<>();
+
+        for(Oferta o : offers) {
             double maxWeight = o.getPesomax();
             double currentWeight = 0;
             for(Paquete p : packages) {
                 if(currentWeight + p.getPeso() <= maxWeight) {
-                    offers.put(o,p);
+                    result.add(p);
+                    result.add(o);
                     currentWeight += p.getPeso();
                     packages.remove(p);
                 }
             }
         }
 
-        return offers;
+        return result;
     }
 }
