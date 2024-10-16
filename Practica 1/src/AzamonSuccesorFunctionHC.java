00e805e24264951aed1140a6ba2371941c0d19e2
Aiden <alexismonroy8@gmail.com>
2024-10-16 15:43:01 +0200

    Partially Implemented SuccesorFunctions

--- Practica 1/src/AzamonSuccesorFunctionHC.java
+++ Practica 1/src/AzamonSuccesorFunctionHC.java
@@ -1,1 +1,25 @@
-public class AzamonSuccesorFunctionHC {}
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AzamonSuccessorFunction implements SuccessorFunction {
+    public List getSuccessors(Object aState) {
+        ArrayList retVal = new ArrayList();
+        AzamonBoard board  = (AzamonBoard) aState;
+        AzamonHeuristicFunction AHF = new AzamonSuccessorFunction();
+
+        for (int i = 0; i <  board.getN_packets(); i++) {
+            for (int j = 0; j < board.getN_offers(); j++) {
+                AzamonBoard newBoard = new AzamonBoard(board.getN_packets(), board.getN_offers(), board.getAssigment());
+                newBoard.move(i,j);
+
+                double v; // = APHF.getHeuristicValue(newBoard);
+                String S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+
+                retVal.add(new Successor(S, newBoard));
+            }
+        }
+        return retVal;
+    }
+}
