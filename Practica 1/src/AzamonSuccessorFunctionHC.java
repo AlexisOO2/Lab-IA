f4d6b9990a661bca00fcaa21e3817eea2f220b85
Aiden <alexismonroy8@gmail.com>
2024-10-21 03:27:11 +0200

    Completed Succesor Function Operators

--- Practica 1/src/AzamonSuccessorFunctionHC.java
+++ Practica 1/src/AzamonSuccessorFunctionHC.java
@@ -2,6 +2,7 @@ import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class AzamonSuccessorFunctionHC implements SuccessorFunction {
     Operation operation;
@@ -15,29 +16,83 @@ public class AzamonSuccessorFunctionHC implements SuccessorFunction {
         AzamonBoard board  = (AzamonBoard) aState;
 
         AzamonHeuristicFunction AHF = new AzamonHeuristicFunction();
-
+        Random rand = new Random();
         for (int i = 0; i <  board.getN_packets(); i++) {
             for (int j = 0; j < board.getN_offers(); j++) {
                 AzamonBoard newBoard = new AzamonBoard(board.getAssignment(), board.getTrans(), board.getPakgs());
                 String S = "";
-
                 switch (operation) {
                     case MOVE: {
+                        i=rand.nextInt(board.getN_packets());
+                        j=rand.nextInt(board.getN_offers());
                         newBoard.move(i,j);
                         double v = AHF.getHeuristicValue(newBoard);
                         S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
                         break;
                     }
                     case MOVE_AND_SWAP: {
+                        int op = rand.nextInt(1);
+                        if (op == 0) {
+                            i = rand.nextInt(board.getN_packets());
+                            j = rand.nextInt(board.getN_offers());
+                            newBoard.move(i, j);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
+                        else {
+                            i = rand.nextInt(board.getN_packets());
+                            j = rand.nextInt(board.getN_packets());
+                            newBoard.swap(i, j);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Swap packet(" + i + ") with packet (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
                         break;
                     }
                     case MOVE_AND_POUR: {
+                        int op = rand.nextInt(1);
+                        if (op == 0) {
+                            i = rand.nextInt(board.getN_packets());
+                            j = rand.nextInt(board.getN_offers());
+                            newBoard.move(i, j);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
+                        else {
+                            i = rand.nextInt(board.getN_offers());
+                            newBoard.pour(i);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Pour offer(" + i + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
                         break;
                     }
                     case MOVE_SWAP_AND_POUR: {
+                        int op = rand.nextInt(2);
+                        if (op == 0) {
+                            i = rand.nextInt(board.getN_packets());
+                            j = rand.nextInt(board.getN_offers());
+                            newBoard.move(i, j);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                            break;
+                        }
+                        else if (op == 1) {
+                            i = rand.nextInt(board.getN_packets());
+                            j = rand.nextInt(board.getN_packets());
+                            newBoard.swap(i, j);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Swap packet(" + i + ") with packet (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
+                        else if (op == 2) {
+                            i = rand.nextInt(board.getN_offers());
+                            newBoard.pour(i);
+                            double v = AHF.getHeuristicValue(newBoard);
+                            S = "Pour offer(" + i + ") Cost =" + v + ") ---> " + newBoard.toString();
+                        }
                         break;
                     }
                     default: {  // Move
+                        i = rand.nextInt(board.getN_packets());
+                        j = rand.nextInt(board.getN_offers());
                         newBoard.move(i,j);
                         double v = AHF.getHeuristicValue(newBoard);
                         S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
--- Practica 1/src/AzamonSuccessorFunctionSA.java
+++ Practica 1/src/AzamonSuccessorFunctionSA.java
@@ -18,28 +18,82 @@ public class AzamonSuccessorFunctionSA implements SuccessorFunction {
         int i,j;
         Random rand = new Random();
 
-        i=rand.nextInt(board.getN_packets());
-        j=rand.nextInt(board.getN_offers());
+
 
         AzamonBoard newBoard = new AzamonBoard(board.getAssignment(), board.getTrans(), board.getPakgs());
         String S = "";
         switch (operation) {
             case MOVE: {
+                i=rand.nextInt(board.getN_packets());
+                j=rand.nextInt(board.getN_offers());
                 newBoard.move(i,j);
                 double v = AHF.getHeuristicValue(newBoard);
                 S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
                 break;
             }
             case MOVE_AND_SWAP: {
+                int op = rand.nextInt(1);
+                if (op == 0) {
+                    i = rand.nextInt(board.getN_packets());
+                    j = rand.nextInt(board.getN_offers());
+                    newBoard.move(i, j);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
+                else {
+                    i = rand.nextInt(board.getN_packets());
+                    j = rand.nextInt(board.getN_packets());
+                    newBoard.swap(i, j);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Swap packet(" + i + ") with packet (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
                 break;
             }
             case MOVE_AND_POUR: {
+                int op = rand.nextInt(1);
+                if (op == 0) {
+                    i = rand.nextInt(board.getN_packets());
+                    j = rand.nextInt(board.getN_offers());
+                    newBoard.move(i, j);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
+                else {
+                    i = rand.nextInt(board.getN_offers());
+                    newBoard.pour(i);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Pour offer(" + i + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
                 break;
             }
             case MOVE_SWAP_AND_POUR: {
+                int op = rand.nextInt(2);
+                if (op == 0) {
+                    i = rand.nextInt(board.getN_packets());
+                    j = rand.nextInt(board.getN_offers());
+                    newBoard.move(i, j);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                    break;
+                }
+                else if (op == 1) {
+                    i = rand.nextInt(board.getN_packets());
+                    j = rand.nextInt(board.getN_packets());
+                    newBoard.swap(i, j);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Swap packet(" + i + ") with packet (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
+                else if (op == 2) {
+                    i = rand.nextInt(board.getN_offers());
+                    newBoard.pour(i);
+                    double v = AHF.getHeuristicValue(newBoard);
+                    S = "Pour offer(" + i + ") Cost =" + v + ") ---> " + newBoard.toString();
+                }
                 break;
             }
             default: {  // Move
+                i = rand.nextInt(board.getN_packets());
+                j = rand.nextInt(board.getN_offers());
                 newBoard.move(i,j);
                 double v = AHF.getHeuristicValue(newBoard);
                 S = "Move packet(" + i + ") to offer (" + j + ") Cost =" + v + ") ---> " + newBoard.toString();
